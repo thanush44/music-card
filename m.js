c70d190b8d7f8345027d524fa68e0138ef2336cf
Thanush <thanushnaik1809@gmail.com>
2026-09-17 12:35:49 +0530

    first commit

---
+++ m.js
@@ -0,0 +1,7 @@
+
+    const card = document.querySelector(".card");
+    const music = document.querySelector(".mu");
+
+    card.addEventListener("click", function () {
+        music.play();
+    });
